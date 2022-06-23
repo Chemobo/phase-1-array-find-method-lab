148446f41c54d771492ed98e297b0ea303dfab86
James Chemobo <jkchemobo@gmail.com>
2022-06-23 11:37:05 +0300

    commit tree 965221701be7dc8b451b608e4225d34bc709887e

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+
+function superbowlWin(record){
+    const recordResult = record.find(recordObject=> recordObject.result == "W")
+    if (recordResult === undefined){
+    }
+    else{
+      return recordResult.year;
+    }
+  }
